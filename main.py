2a0b3aecc3b03b697bf740f4f5af3be2b72ab3aa
bulya <buleychenko29121998@gmail.com>
2023-04-25 20:25:11 +0200

    homework

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-a = float(input("Длина стороны a: "))
-b = float(input("Длина стороны b: "))
-c = float(input("Длина стороны c: "))
-
-p = (a + b + c) / 2
-
-s = (p * (p - a) * (p - b) * (p - c)) ** 0.5
-
-print("Площадь треугольника равна", s)
+number = int(input("Введите целое число: "))
+if number % 2 != 0:
+    if number > 20:
+        print("Not Weird")
+    else:
+        print("Weird")
+elif number >= 2 and number <= 5:
+    print("Not Weird")
+elif number >= 6 and number <= 20:
+    print("Weird")
+else:
+    print("Not Weird")

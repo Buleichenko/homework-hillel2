107b172f93ed7636e6266870828c0f8c7706486a
bulya <buleychenko29121998@gmail.com>
2023-04-30 21:44:15 +0200

    homework

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-number = int(input("Enter the number: "))
-if number % 2 != 0:
-    if number > 20:
-        print("Not Weird")
-    else:
-        print("Weird")
-elif number >= 2 and number <= 5:
-    print("Not Weird")
-elif number >= 6 and number <= 20:
-    print("Weird")
-else:
-    print("Not Weird")
+n = int(input("Введите число N: "))
+i = 0
+while i <= n:
+    if i % 3 == 0:
+        print(i)
+    i += 1
+
+n = int(input("Введите число N: "))
+i = 0
+summa = 0
+while i <= n:
+    if i % 3 == 0:
+        summa += i
+    i += 1
+print("Числа от 0 до N, которые делятся на 3 без остатка: ", summa)
